d1dc3805df2e76c2685ab549bcc24b7d6314156e
pavel-paulau <pavel.paulau@gmail.com>
2014-11-03 23:06:50 -0800

    Implement naïve matrix multiplication

---
+++ matrix.go
@@ -0,0 +1,15 @@
+package alco
+
+func multiplyIterative(a, b [][]int) [][]int {
+	n := len(a)
+	c := make([][]int, n)
+	for i := 0; i < n; i++ {
+		c[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			for k := 0; k < n; k++ {
+				c[i][j] += a[i][k] * b[k][j]
+			}
+		}
+	}
+	return c
+}
---
+++ matrix_test.go
@@ -0,0 +1,29 @@
+package alco
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIterativeMultiplication(t *testing.T) {
+	a := [][]int{
+		[]int{1, 2, 5, 4},
+		[]int{1, 1, 3, 8},
+		[]int{7, 3, 6, 9},
+		[]int{2, 5, 4, 7},
+	}
+	b := [][]int{
+		[]int{9, 8, 1, 7},
+		[]int{2, 7, 2, 8},
+		[]int{1, 5, 4, 3},
+		[]int{2, 3, 6, 7},
+	}
+	c := [][]int{
+		[]int{26, 59, 49, 66},
+		[]int{30, 54, 63, 80},
+		[]int{93, 134, 91, 154},
+		[]int{46, 92, 70, 115},
+	}
+	assert.Equal(t, c, multiplyIterative(a, b))
+}
